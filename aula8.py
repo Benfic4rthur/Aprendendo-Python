25c873c9259d29e39aa704c63005ed5f11740660
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-23 14:53:10 -0300

    sequencia nas aulas

---
+++ aula8.py
@@ -0,0 +1,12 @@
+nome = input('Nome: ')
+sobrenome = input('Sobrenome: ')
+idade = input('Idade: ')
+datanascimento = input('Data de nascimento: ')
+maior = int(idade) > 18
+altura = input('Altura: ')
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Data de mascimento:', datanascimento)
+print('É maior de idade?', maior)
+print('Altura:', altura)
