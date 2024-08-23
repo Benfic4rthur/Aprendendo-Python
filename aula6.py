a68ad7ecef4854db227cab00fe9d74215198461d
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-23 13:33:40 -0300

    aula 6 completa

---
+++ aula6.py
@@ -0,0 +1,4 @@
+# converter valores entre tipos
+# print('1' + 1) geraria um erro
+print(int('1') + 1) # o int converte o valor para inteiro e pode ser feita a soma
+print('1' + str(1)) # o str converte o valor para string e pode ser feita a concatenação
