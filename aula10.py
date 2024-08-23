3d4fa78a03974bdc54bc5ffd75ad3fd4774c9135
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-23 15:23:05 -0300

    aula de prescedencia.

---
+++ aula10.py
@@ -0,0 +1,8 @@
+concatenacao = 'ola ' + 'mundo'
+print(concatenacao)
+
+a_dez_vezes = 'a' * 10
+print(a_dez_vezes)
+
+tres_vezes_arthur = 3 * 'arthur'
+print(tres_vezes_arthur)
---
+++ aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 5
+print(adicao)
+
+subtracao = 10 - 5
+print(subtracao)
+
+multiplicacao = 10 * 5
+print(multiplicacao)
+
+divisao = 10 / 5 # sempre retorna um numero float
+print(divisao)
+
+divisao_inteira = 10 // 5 # trunca o número
+print(divisao_inteira)
+
+exponenciacao = 10 ** 5
+print(exponenciacao)
+
+resto = 10 % 5 # retorna o resto pra saber se é par ou impar
+print(resto)
