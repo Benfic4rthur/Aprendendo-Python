33eb60725df9915241a6975f6da94a30d8dffc5f
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-23 13:12:01 -0300

    avanço nas aulas.

---
+++ aula4.py
@@ -0,0 +1,6 @@
+print(1) # variavel do tipo inteiro
+print(-10) # variavel do tipo inteiro
+print(1.0) # variavel do tipo float
+print(type(1.0))# a função type retorna o tipo da variável
+print(type(1))
+print(type('1.0'))
---
+++ aula5.py
@@ -0,0 +1,8 @@
+sim = True
+nao = False
+print(type(sim))
+print(type(nao))
+valor = 15
+valor2 = 10
+print(valor == valor2) # False
+print(valor != valor2) # True
