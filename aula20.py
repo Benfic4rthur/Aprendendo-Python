1c082bde3a2699fc001ceb2c68d09a1f2ba180cc
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-24 10:37:40 -0300

    operador not

---
+++ aula20.py
@@ -0,0 +1,9 @@
+# operador logico not
+# senha = input('digite a senha: ')
+
+# if not senha:
+#     print('Favor informar a senha')
+
+# o not tambem inverte o valor True para False e False para True
+print(not True) # False
+print(not False) # True
