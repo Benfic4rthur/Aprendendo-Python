1322703cc43e2d5d783cd7a3244a336f7f8cd297
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-24 09:21:42 -0300

    aula 15

---
+++ aula15.py
@@ -0,0 +1,5 @@
+numero1 = int(input('digite um numero: ')) # se eu passar o tipo de variavel antes do input, ele permite que o input seja convertido e calculado caso seja um numero
+numero2 = int(input('digite outro numero: '))
+
+soma = numero1 + numero2
+print('a soma dos dois valores e: ', soma)
