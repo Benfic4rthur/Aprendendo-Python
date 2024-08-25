0bec583ed5839b03bd0685cac52e87b2248f064d
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-25 09:48:34 -0300

    constantes

---
+++ aula27.py
@@ -0,0 +1,6 @@
+# constantes 
+# no python não existem constantes, mas existe uma convesão para isso que é usar letras maiusculas
+radar = 30 # variavel "normal"
+RADAR = 50 # variavel "constante"
+
+#usa-se as letras maiusculas para que outras pessoas e até mesmo você trate aquela variavel como constante
