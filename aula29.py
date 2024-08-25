bb641afa1131d73dfee77c190c4e51c6561163b1
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-25 10:55:33 -0300

    is e is not none

---
+++ aula29.py
@@ -0,0 +1,14 @@
+#flag = marcar local
+#None = não valor
+#is e is not = é ou não é
+#id = identificador de variavel
+
+condicao = False
+passounoif = None
+if condicao:
+    passounoif = True
+    print('verdadeiro')
+else:
+    print('falso')
+print(passounoif, passounoif is None) # checa se a variavel passounoif e None
+print(passounoif, passounoif is not None) # checa se a variavel passounoif não e None
