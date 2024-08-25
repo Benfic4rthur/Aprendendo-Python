1588b3a08509a7a9e4cca3d1db6a75275e11900c
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-25 10:28:18 -0300

    aprendendo ids nas variaveis

--- aula28.py
+++ aula28.py
@@ -1,3 +1,5 @@
 # verificando id de elementos
 v1 = 5
-print(id(v1))
+v2 = 5
+print(id(v1)) 
+print(id(v2)) # os valores de id serão iguais pois os valores das variaveis são iguais, e isso pode gerar erros
