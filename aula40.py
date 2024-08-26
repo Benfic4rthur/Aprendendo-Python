f0c94a06c6dd923efbdb14fedc9379e7bc83d531
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-26 15:31:05 -0300

    for e range

---
+++ aula40.py
@@ -0,0 +1,7 @@
+# For + Range
+# range -> (start, stop, step) Caso coloque apenas um valor, esse valor sera o stop e o start será 0 e step sendo 1
+
+numeros = range(0, 30, 10)
+
+for numero in numeros:
+    print(numero)  # ira imprimir 0 10 20
