1c54ff097988569f241a38aa6b51937f36c5319e
Benfic4rthur <arthur_benfica@hotmail.com>
2024-08-26 10:32:39 -0300

    while e continue

--- aula33.py
+++ aula33.py
@@ -1,7 +1,6 @@
 # while e break
 numero = 0
-ultimonum = 15
 
-while numero <= ultimonum:
-    numero = numero + 1
+while numero < 10:
+    numero += 1
     print(numero)
---
+++ aula34.py
@@ -0,0 +1,9 @@
+# while + continue
+numero = 0
+
+while numero < 20:
+    numero += 1
+    print(numero)
+    if numero == 10:
+        continue
+print("a contagem acabou")
